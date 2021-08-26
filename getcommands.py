5e688d15ab04923062fc0b6791bd42cbb861feab
epcnes <kaitlyngriffin99@gmail.com>
2021-08-26 12:25:38 -0400

    a random commit

--- getcommands.py
+++ getcommands.py
@@ -3,13 +3,15 @@ import requests; import lxml.html as lh; import bs4
 
 url = "https://www.onemathematicalcat.org/MathJaxDocumentation/TeXSyntax.htm"
 commands = []
+commentindex =[]
+i = 0
 
 r = requests.get(url)
 soup = bs(r.content, 'lxml')
 html = list(soup.children)[2]
-body = list(html.children)[3]
-table = list(body.children)[16]
-# cells = list(table.children)[0]
+body = list(html.children)[5]
+table = list(body.children)
+cells = list(table.children)[0]
 
-print (table)
-# print([type(item) for item in list(cells.children)])
+# print([type(item) for item in list(body.children)])
+print (cells)
