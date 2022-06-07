216c63b9edc1f212656624a908d4aa0b234a2c87
epcnes <kaitlyngriffin99@gmail.com>
2022-06-07 12:26:10 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# packages used just in case
+#!pip install googlesearch-python; !pip install requests; !pip install bs4, !pip install wikipedia
+
+import wikipedia; from bs4 import BeautifulSoup
+
+formula = str(input("Enter formula name: "))
