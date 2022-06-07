dcde7a160c6c879aa6d56be55ba34cd1481abd08
epcnes <kaitlyngriffin99@gmail.com>
2022-06-07 15:58:06 -0400

    printing formulas in latex

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 # packages used just in case
 #!pip install googlesearch-python; !pip install requests; !pip install bs4, !pip install wikipedia
 
-import wikipedia; from bs4 import BeautifulSoup
+from sympy import print_latex; import sympy
+import wikipedia
+from bs4 import BeautifulSoup as bs
+from collections import defaultdict
 
-formula = str(input("Enter formula name: "))
+#getting formula name and wikipedia page#
+searchFor = str(input("Enter formula name: ")) + " equation"
+topic = wikipedia.page(searchFor,auto_suggest=False)
+equations = bs(topic.html(),"html.parser").find_all('annotation')
+
+#make formula pretty and print it#
+equation = equations[-1].text.split("{\displaystyle ")[1][:-1]
+print_latex(equation)
