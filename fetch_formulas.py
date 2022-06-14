c62d378b70fdc97d580cc0f721e05938c313008f
epcnes <kaitlyngriffin99@gmail.com>
2022-06-14 17:08:54 -0400

    python file indead of json

--- fetch_formulas.py
+++ fetch_formulas.py
@@ -1,28 +1,19 @@
 # packages used just in case
 #!pip install googlesearch-python; !pip install requests; !pip install bs4, !pip install wikipedia
 
-from collections import defaultdict
-from pprint import pprint
 import wikipedia
 from bs4 import BeautifulSoup as bs
 import googlesearch as ggl
 import requests
-import json
+from formulas import formulas
 
 #definitions#
 correct = False
 i = 0
-f = open("formulas.json", "r")
-def append_json(new_data, filename = 'formulas.json'):
-    with open(filename, 'r+') as file:
-        file_data = json.load(file)
-        file_data["formulas"].append(new_data)
-        file.seek(0)
-        json.dump(file_data, file, indent=4)
 
 #getting formula name and wikipedia page#
 Inquiry = input("Enter formula name: "); inquiry = Inquiry + " equation wikipedia"
-#make database of successful searches (formulas.json) and common formulas (if/else)#
+#make database of successful searches (formulas.py) and common formulas (if/else)#
 searchResult = ggl.search(inquiry, num=1, stop=1, tld="com")
     #get that url for the actual wikipedia page lmao#
 for url in searchResult:
@@ -38,18 +29,18 @@ while correct == False:
         equation = equations[i].text.split("{\displaystyle ")[1][:-1]
         equation = equation.replace("{",""); equation = equation.replace("}","")
         correct = input(f"{equation}:\nis this correct? enter y or n: ")
+        equation = str(equation)
         if correct == 'y':
             correct = True
             i = len(equations)
-            entry = {
-                'name' : f'{Inquiry.lower}', 
-                'formula' : f'{equation}'
-                }
+            entry = (Inquiry,equation)
         else:
             correct = False
         i+=1
 
-append_json(entry)
+f = open("formulas.py", "a")
+f.write(f"{formulas.append(entry)}")
+f.close()
 
 #write .tex file#
 f = open('formula.tex', 'w')
---
+++ formulas.py
@@ -0,0 +1,3 @@
+formulas = [
+            ("pythagorean", "a^2 + b^2 = c^2"),
+            ('gauss', '\\Phi _E=\\frac Q\\varepsilon _0')]
